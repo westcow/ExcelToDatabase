d1acba37f83f4ef571be9d04df8b57c6acccb198
westcowboy <1024668091@qq.com>
2021-04-11 18:10:28 +0800

    first commit

---
+++ src/main/java/com/bjb/exceltodb/ExceltodbApplication.java
@@ -0,0 +1,13 @@
+package com.bjb.exceltodb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExceltodbApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ExceltodbApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/bjb/exceltodb/ExceltodbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bjb.exceltodb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExceltodbApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
